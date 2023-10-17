0d475d2cb516999e0a91a943307daeeba2d31b06
Gigantorz <69411983+Gigantorz@users.noreply.github.com>
2023-10-17 11:56:34 -0600

    Started React website with Node.js

---
+++ portfolio-trial-app/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const portfolio_announce = <h1>Hello! We are going to begin our React.js journey with this portfolio.</h1>
+root.render(portfolio_announce);
